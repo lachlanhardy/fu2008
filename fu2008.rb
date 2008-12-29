268a387d5c2d523e8b811cd73b98b37f590b15f1
Lachlan Hardy <lachlan@lachstock.com.au>
2008-12-29 14:46:07 +1100

    separating controller and view

--- fu2008.rb
+++ fu2008.rb
@@ -3,9 +3,10 @@ require 'sinatra'
 require 'twitter'
 require 'haml'
 
+use_in_file_templates!
+
+
 get '/' do
-  
-  
+  @results = Twitter::Search.new(' "fuck 2008" OR "fuck you 2008" OR "2008 sucks"')
   haml :index
-  
 end

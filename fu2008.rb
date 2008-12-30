4fb20a93342165f87f9cc90e7d657e3dceaef9d9
Lachlan Hardy <lachlan@lachstock.com.au>
2008-12-30 23:24:09 +1100

    adding sexy new logo

--- fu2008.rb
+++ fu2008.rb
@@ -3,15 +3,18 @@ require 'sinatra'
 require 'twitter'
 require 'haml'
 
-get '/' do
-  @results = Twitter::Search.new(' "fuck 2008" OR "fuck you 2008" OR "2008 sucks"')
-  @results.per_page(100)
-  haml :index, :options => {:format => :html4,
-                            :attr_wrapper => '"'}
-end
-
 # stylesheet
 get '/stylesheets/main.css' do
   header 'Content-Type' => 'text/css; charset=utf-8'
   css :main
 end
+
+# homepage
+get '/' do
+  @results = Twitter::Search.new(' "fuck 2008" OR "fuck you 2008" OR "2008 sucks" -filter:links')
+  @results.per_page(100)
+  
+  haml :index, :options => {:format => :html4,
+                            :attr_wrapper => '"'}
+end
+

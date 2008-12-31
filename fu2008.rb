d2ebb016339328eb945cd5feb400cf2d3fed308c
Lincoln Stoll <lstoll@lstoll.net>
2008-12-31 14:47:22 +1100

    Adding a very simple, not very threadsafe 5 minute cache on index.

--- fu2008.rb
+++ fu2008.rb
@@ -17,33 +17,43 @@ end
 
 # homepage
 get '/' do
-  @blacklisted_strings = []
-  @results = []
-  # read blacklist file.
-  File.open(File.join(File.dirname(__FILE__), '/blacklist.txt'), 'r') do |file|
-    while line = file.gets  
-        @blacklisted_strings << line.strip 
-    end  
-  end
-  
-  @search = Twitter::Search.new(' "fuck 2008" OR "fuck you 2008" OR "2008 sucks" OR #2008sux0r')
-  @search.per_page(100)
-  @search.each do |item|
-    unless @blacklisted_strings.any? {|i| item["text"].downcase.match(i.downcase)}
-        @results << item["text"].gsub(/^@\w[a-z]+\s/, '').
-          gsub(/((ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?)/i, '<a href="\1">\1</a>').
-          gsub(/(fuck you 2008\W?|fuck 2008\W?|2008 sucks\W?|#2008sux0r)/i, '<strong>\1</strong>').
-          gsub(/(fuck yeah 2009\W?)/i, '<em>\1</em>')
+  if $last_updated + 300 < Time.now # hasn't been updated for 5 mins. - 300 secs
+    p "updating cache"
+    @blacklisted_strings = []
+    @results = []
+    # read blacklist file.
+    File.open(File.join(File.dirname(__FILE__), '/blacklist.txt'), 'r') do |file|
+      while line = file.gets  
+          @blacklisted_strings << line.strip 
+      end  
     end
+  
+    @search = Twitter::Search.new(' "fuck 2008" OR "fuck you 2008" OR "2008 sucks" OR #2008sux0r')
+    @search.per_page(100)
+    @search.each do |item|
+      unless @blacklisted_strings.any? {|i| item["text"].downcase.match(i.downcase)}
+          @results << item["text"].gsub(/^@\w[a-z]+\s/, '').
+            gsub(/((ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?)/i, '<a href="\1">\1</a>').
+            gsub(/(fuck you 2008\W?|fuck 2008\W?|2008 sucks\W?|#2008sux0r)/i, '<strong>\1</strong>').
+            gsub(/(fuck yeah 2009\W?)/i, '<em>\1</em>')
+      end
+    end
+    
+    Cache["index"] = haml :index, :options => {:format => :html4,
+                              :attr_wrapper => '"'}
+    $last_updated = Time.now
   end
-  
-  
-  haml :index, :options => {:format => :html4,
-                            :attr_wrapper => '"'}
+  Cache["index"]
 end
 
 # colophon
 get '/colophon' do
   haml :colophon, :options => {:format => :html4,
                             :attr_wrapper => '"'}
+end
+
+# Configure Block.
+configure do
+  Cache = {}      # Create a new cache
+  $last_updated = Time.now - 100000 # ensure the first request is outdated.
 end

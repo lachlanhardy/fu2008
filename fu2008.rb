f2877bfab037391886cbb082b198d019d8f670d1
Lincoln Stoll <lstoll@lstoll.net>
2008-12-31 14:20:22 +1100

    Blacklist in external file.
    Moving blacklisted strings from array to blacklist.txt - so the blacklist can be updated without changing the code. Currently opened on every request - TODO - either cache (for update on deploy), or move to shared (check deploy script - for updates on server)

--- fu2008.rb
+++ fu2008.rb
@@ -3,8 +3,6 @@ require 'sinatra'
 require 'twitter'
 require 'haml'
 
-BLACKLISTED_STRINGS = ["Office 2008", "server 2008", "xxx", "MacOffice", "Online information 2008", "sure like to fuck you 2008"]
-
 # reset stylesheet
 get '/stylesheets/reset.css' do
   header 'Content-Type' => 'text/css; charset=utf-8'
@@ -19,15 +17,23 @@ end
 
 # homepage
 get '/' do
+  @blacklisted_strings = []
   @results = []
+  # read blacklist file.
+  File.open(File.join(File.dirname(__FILE__), '/blacklist.txt'), 'r') do |file|
+    while line = file.gets  
+        @blacklisted_strings << line.strip 
+    end  
+  end
+  
   @search = Twitter::Search.new(' "fuck 2008" OR "fuck you 2008" OR "2008 sucks" OR #2008sux0r')
   @search.per_page(100)
   @search.each do |item|
-    unless BLACKLISTED_STRINGS.any? {|i| item["text"].match(i)}
-      @results << item["text"].gsub(/^@\w[a-z]+\s/, '').
-        gsub(/((ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?)/i, '<a href="\1">\1</a>').
-        gsub(/(fuck you 2008\W?|fuck 2008\W?|2008 sucks\W?|#2008sux0r)/i, '<strong>\1</strong>').
-        gsub(/(fuck yeah 2009\W?)/i, '<em>\1</em>')
+    unless @blacklisted_strings.any? {|i| item["text"].downcase.match(i.downcase)}
+        @results << item["text"].gsub(/^@\w[a-z]+\s/, '').
+          gsub(/((ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?)/i, '<a href="\1">\1</a>').
+          gsub(/(fuck you 2008\W?|fuck 2008\W?|2008 sucks\W?|#2008sux0r)/i, '<strong>\1</strong>').
+          gsub(/(fuck yeah 2009\W?)/i, '<em>\1</em>')
     end
   end
   
